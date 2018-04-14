22b9dedb42956060dfadc3cda713f6f692cfa713
Yui Hirasawa <10993186+akiyamn@users.noreply.github.com>
2018-04-15 03:35:23 +1000

    Added an argument parsing system

--- README.md
+++ README.md
@@ -3,23 +3,44 @@
 A simple screenshot script/program built for GNU/Linux which uploads to the [slimecorp.biz/i](http://slimecorp.biz/i) platform.
 Written for the GNU/Linux platform, based on Python 3.6.
 
+This program was inspired by the selective screenshot and upload feature of popular
+Windows exclusive programs such as ShareX and puush.
 
-## Usage
-`python3 slimeshot.py`
+
+## Basic Usage
+Given execution permissions, simply:
+
+`./slimeshot`
 
 A file must be created in the main directory, called `key.txt`.
-This file must include only one line, which is a valid key
+This file must include only one line, which is a valid keyFile
 for the [slimecorp.biz/i](http://slimecorp.biz/i) platform to function correctly.
 
+On the first launch, slimeshot will create a file named `key.txt` if it doesn't exist.
 
-## Depends on
 
+## Dependencies
 ### Packages
 -   python3
 -   maim
--   xclip (required by maim)
--   pip (to install Python libs)
+-   xclip
+-   python3-pip (to install Python libs)
 
 ### Python Libs
 -   request
 -   playsound
+
+
+## Installation
+
+All packages shown above can be installed using the package manager relevent to your distro.
+On Debain based systems this would be achieved by running:
+
+`sudo apt-get install python3 maim xclip python3-pip`
+
+Every "python lib" listed must be installed using pip. This can be done by running:
+
+`pip3 install request playsound`
+
+After downloading this git repository you may extract the file and run the program
+as outlined above in the "Basic Usage" section.
--- slimeshot.py
+++ slimeshot.py
@@ -1,22 +1,38 @@
 #!/usr/bin/python3
 
+__version__ = "0.2.0"
+
 import subprocess
 import requests
 from playsound import playsound
 import os
 import sys
+import argparse
 
 URL = 'http://slimecorp.biz/i/pics.php'
+
+# Argument Parsing
+parser = argparse.ArgumentParser(description="A simple Python-based screenshot program for slimecorp.biz/i.")
+parser.add_argument("--version", action="version", version="%(prog)s " + __version__)
+parser.add_argument("-q", "--quiet", help="mutes all sound, text and notification output", action='store_true')
+parser.add_argument("--dryrun", help="takes a screenshot without uploading it", action='store_true')
+parser.add_argument("--silent", help="disables audio output", action='store_true')
+args = parser.parse_args()
+
+if args.quiet:
+    args.silent = True
 
 
 # Sends a GNU/Linux notification via 'notify-send'
 def notify(title, text="", icon=""):
-    subprocess.Popen(["notify-send", "-i", icon, title, text])
+    if not args.quiet:
+        subprocess.Popen(["notify-send", "-i", icon, title, text])
 
 
 # Sends an error notification to the user via 'notify-send'
 def showError(error):
     notify("Error", error)
+    sys.exit(1)
 
 
 # Sends 'data' to the clipboard
@@ -34,9 +50,11 @@ def clip():
     err = err.decode()
     if err == "":
         return True
+    elif "right-click" in err:
+        sys.exit(0)
     else:
-        showError("Error" , "Failed to clip the specified region.\n" + err)
-        return False
+        showError('Failed to clip the specified region.\n' + err)
+    return False
 
 
 # Sends a POST request to the screenshot server including the screenshot and the key
@@ -58,20 +76,23 @@ if os.path.exists("key.txt"):   # Reads key from key.txt if no errors
     except IOError as e:
         showError("IOError: " + e)
 else:   # Display error and create new file if key.txt doesn't exist
-    showError("Please add a key to your key.txt file!")
     with open("key.txt", "w"): pass
-    sys.exit(1)
+    showError("Please add a key to your key.txt file!")
+
 
 message = ""
-error = ""
 
 if clip():
-    message = post()
+    if not args.dryrun:
+        message = post()
+    else:
+        message = "DRY RUN"
 
 if message != "You fucked up." and message != "":   # Server returns successful status
     clipboard(message)
     notify("Screenshot successful!", message, thisDir + "/temp.png")
-    playsound("success.wav")
+    if not args.silent:
+        playsound("success.wav")
 else:   # Server denies screenshot, show error
     showError("Server failed to upload the screenshot.")
     sys.exit(1)
